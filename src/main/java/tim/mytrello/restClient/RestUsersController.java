d43c8137bd5480ce2d62df7e2b021afac3d4936c
Timur Badretdinov <tim_baton@mail.ru>
2019-08-07 08:18:45 +0300

    Added method for getting users' events

--- src/main/java/tim/mytrello/restClient/RestUsersController.java
+++ src/main/java/tim/mytrello/restClient/RestUsersController.java
@@ -3,10 +3,14 @@ package tim.mytrello.restClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tim.mytrello.Dto.EventDto;
+import tim.mytrello.Dto.UserDto;
+import tim.mytrello.entity.Event;
 import tim.mytrello.entity.Users;
 import tim.mytrello.repository.UserRepository;
 import tim.mytrello.service.UserService;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -39,11 +43,27 @@ public class RestUsersController {
             return userByLogin.get();
         } else throw new IllegalArgumentException("User not found");
     }
-    @GetMapping(value = "/rest/users/", params = "id")
-    public Users getUserByLogin(@RequestParam(name = "id") int id) {
+
+    @GetMapping(value = "/rest/users", params = "id")
+    public UserDto getUserByLogin(@RequestParam(name = "id") int id) {
         Optional<Users> userById = userRepository.findUserById(id);
         if (userById.isPresent()) {
-            return userById.get();
+            return UserDto.from(userById.get());
+        } else throw new IllegalArgumentException("User not found");
+    }
+
+    @GetMapping(value = "/rest/event", params = "user_id")
+    public List<EventDto> getUserEvents(@RequestParam(name = "user_id") int id) {
+        Optional<Users> userById = userRepository.findUserById(id);
+        if (userById.isPresent()) {
+            Users user = userById.get();
+            List<Event> events = user.getEvents();
+            List<EventDto> eventDtos = new LinkedList<>();
+
+            for (Event event : events) {
+                eventDtos.add(EventDto.from(event));
+            }
+            return eventDtos;
         } else throw new IllegalArgumentException("User not found");
     }
 }
